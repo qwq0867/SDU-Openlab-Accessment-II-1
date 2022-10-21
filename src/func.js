1e220b49416b6eb927ffe039c45eab24aa3abb98
Eilphy_ <75255501+qwq0867@users.noreply.github.com>
2022-10-21 19:50:55 +0800

    0.9

--- src/func.js
+++ src/func.js
@@ -2,11 +2,15 @@ var player;
 var playBtn;
 var pauseBtn;
 var isPlaying; 
+var file;
+var source;
 window.onload=function()
 {
     player=document.getElementById("player");
     playBtn=document.getElementById("playBtn");
     pauseBtn=document.getElementById("pauseBtn");
+    file=document.getElementById("fileSelector");
+    source=document.getElementById("videoSource");
     isPlaying=false;
 }
 
@@ -25,4 +29,12 @@ function pausevideo()
     isPlaying=false;
     pauseBtn.style.display="none";
     playBtn.style.display="flex";
+}
+function loadvideo()
+{
+    
+    //player.src=file.files[0];
+    var myurl=URL.createObjectURL(file.files[0]);
+    source.src=myurl;
+    player.load();
 }
